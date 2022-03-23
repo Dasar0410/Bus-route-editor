ce819166bdfdfb82266a587806a26a92e415b8a7
Markuso <markuols@stud.ntnu.no>
2022-03-23 13:22:25 +0100

    Add: class innhold

---
+++ CPP-Filer/Ruter.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file Ruter.cpp
+ * @brief Ruter kodene
+ * @version 0.1
+ * @date 2022-03-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "Ruter.h"
+#include "Rute.h"
+
+class Ruter{
+private:
+map <int, Rute*> Rutene;
+
+public:
+
+
+}
